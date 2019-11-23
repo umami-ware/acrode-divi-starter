4692aad8c0e580b8ff3fe24ad5be1d4c95caa5d9
JohannesMutz <jmutz+github.com@outlook.de>
2019-11-23 18:40:14 +0100

    Update

--- functions.php
+++ functions.php
@@ -1,33 +1,36 @@
 <?php
-
-/**** Optional: Development Mode ****/
-$GLOBALS['acViewSiteKey'] = 'ac2019';
-$GLOBALS['acReleaseTag'] = 1;
-$GLOBALS['acReleaseMonat'] = 1;
-$GLOBALS['acReleaseJahr'] = 2020;
-$GLOBALS['hideTimer'] = false;
-$GLOBALS['h2Text'] = 'Die Webseite ist aktuell noch in der Entwicklung';
-$GLOBALS['acColorOne'] = '#29a6f6';
-$GLOBALS['acColorTwo'] = '#6251d6';
-include_once(__DIR__ . '/includes/ac_development.php');
-
-/**** Optional: Branding ****/
-include_once(__DIR__ . '/includes/ac_branding.php');
-
-/**** Optional: Add global Divi module anywhere ****/
-function acrode_show_gdivi_module_shortcode($gdId)
-{
-	extract(shortcode_atts(array('id' => '*'), $gdId));
-	return do_shortcode('[et_pb_section global_module="' . $id . '"][/et_pb_section]');
-}
-add_shortcode('acrode_gdivi_module', 'acrode_show_gdivi_module_shortcode');
-
 /**** Necessary Code ****/
+// https://github.com/YahnisElsts/plugin-update-checker v.4.8.1
+require __DIR__ . '/includes/plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://wordpress.acrode.com/wp-update-server/?action=get_metadata&slug=acrode-divi-starter',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'acrode-divi-starter'
+);
 /* acrode theme */
+// custom files
+$acCustomTheme = ABSPATH . '/acrode/theme';
+if ($acCustomTheme) {
+	require ABSPATH . '/wp-content/acrode/theme/custom.php';
+}
+
+if ($acDevelopment) {
+	include_once(__DIR__ . '/includes/ac_development.php');
+}
+if ($acBranding) {
+	include_once(__DIR__ . '/includes/ac_branding.php');
+}
+
 function acrode_enqueue()
 {
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 	wp_enqueue_script('custom-scripts', get_stylesheet_directory_uri() . '/script.js', array('jquery'));
+	// custom files
+	global $acCustomTheme;
+	if ($acCustomTheme) {
+		wp_enqueue_style('parent-style', '/wp-content/acrode/theme/style.css');
+		wp_enqueue_script('custom-scripts', '/wp-content/acrode/theme/script.js', array('jquery'));
+	}
 }
 /* acrode remove type from style tags */
 function acrode_codeless_remove_type_attr($tag)
@@ -71,21 +74,6 @@ function ac_remove_editor_menu() {
 	remove_submenu_page( 'plugins.php','plugin-editor.php' );
 }
 
-/* acrode theme */
-add_action('wp_enqueue_scripts', 'acrode_enqueue');
-/* acrode set filter for style and script tags */
-add_filter('style_loader_tag', 'acrode_codeless_remove_type_attr', 10, 2);
-add_filter('script_loader_tag', 'acrode_codeless_remove_type_attr', 10, 2);
-/* acrode add html sitemap shortcode */
-add_shortcode('acrode_html_sitemap', 'acrode_get_html_sitemap');
-/* acrode remove hEntry filter */
-add_filter('post_class', 'acrode_remove_hentry_class');
-
-/* acrode hide editors */
-if(!isset($_GET['acAdmin'])) {
-	add_action('admin_menu', 'ac_remove_editor_menu', 1);
-}
-
 /* acrode remove pages from search */
 if (!is_admin()) {
 	function wpb_search_filter($query)
@@ -97,3 +85,18 @@ if (!is_admin()) {
 	}
 	add_filter('pre_get_posts', 'wpb_search_filter');
 }
+
+/* acrode hide editors */
+if(!isset($_GET['acAdmin'])) {
+	add_action('admin_menu', 'ac_remove_editor_menu', 1);
+}
+
+/* acrode theme */
+add_action('wp_enqueue_scripts', 'acrode_enqueue');
+/* acrode set filter for style and script tags */
+add_filter('style_loader_tag', 'acrode_codeless_remove_type_attr', 10, 2);
+add_filter('script_loader_tag', 'acrode_codeless_remove_type_attr', 10, 2);
+/* acrode add html sitemap shortcode */
+add_shortcode('acrode_html_sitemap', 'acrode_get_html_sitemap');
+/* acrode remove hEntry filter */
+add_filter('post_class', 'acrode_remove_hentry_class');
